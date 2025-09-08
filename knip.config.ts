56e53e8a3236260694c45a9aaf713d04f8285bd7
Eli <88557639+lishaduck@users.noreply.github.com>
2025-09-08 09:03:24 -0500

    fix: knip:prod
    Ref: https://github.com/webpro-nl/knip/issues/1253#issuecomment-3264712969

--- knip.config.ts
+++ knip.config.ts
@@ -1,4 +1,9 @@
+import process from "node:process";
+
 import type { KnipConfig } from "knip";
+
+const isProductionMode =
+  process.argv.includes("--production") || process.argv.includes("--strict");
 
 export default {
   ignoreExportsUsedInFile: {
@@ -17,12 +22,23 @@ export default {
       project: ["src/**/*.ts!", "!scripts/*.ts!"],
     },
 
+    "packages/define-config": {
+      ignoreDependencies: isProductionMode ? ["eslint"] : [],
+      project: ["src/**/*.ts!", "!scripts/*.ts!"],
+    },
+    "packages/svelte": {
+      ignoreDependencies: isProductionMode ? ["svelte-eslint-parser"] : [],
+      project: ["src/**/*.ts!", "!scripts/*.ts!"],
+    },
+
     "packages/tsconfig": {
       project: ["base.json"],
     },
 
     // Revert `@types/*` packages' project glob.
-    "packages/types__*": {},
+    "packages/types__*": {
+      ignoreDependencies: isProductionMode ? ["eslint"] : [],
+    },
   },
 
   ignoreDependencies: [
