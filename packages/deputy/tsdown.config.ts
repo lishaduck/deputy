54bbe6b13d774be1f09482da05c8809833211b58
Eli <88557639+lishaduck@users.noreply.github.com>
2025-09-08 22:35:37 -0500

    fix: ship dts

--- packages/deputy/tsdown.config.ts
+++ packages/deputy/tsdown.config.ts
@@ -3,6 +3,8 @@ import { tsdownConfig, type UserConfig } from "@eslint-deputy/tsdown-config";
 const _default: UserConfig = tsdownConfig({
   entry: ["src/index.ts", "src/use-at-your-own-risk.ts"],
   unused: false,
+
+  copy: ["src/typegen.d.ts"],
 });
 
 export default _default;
--- packages/svelte/tsdown.config.ts
+++ packages/svelte/tsdown.config.ts
@@ -1,5 +1,7 @@
 import { tsdownConfig, type UserConfig } from "@eslint-deputy/tsdown-config";
 
-const _default: UserConfig = tsdownConfig();
+const _default: UserConfig = tsdownConfig({
+  copy: ["src/typegen.d.ts"],
+});
 
 export default _default;
