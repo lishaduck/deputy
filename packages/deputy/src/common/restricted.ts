982e14874a4889f8aca7c81ce15396cc5a03a1ec
Eli <88557639+lishaduck@users.noreply.github.com>
2025-09-09 09:05:47 -0500

    chore: update sort

--- packages/deputy/src/common/restricted.ts
+++ packages/deputy/src/common/restricted.ts
@@ -7,52 +7,52 @@ import type { RuleConfig as SyntaxRuleConfig } from "eslint-no-restricted/syntax
 export const syntax: SyntaxRuleConfig[] = [
   {
     name: "no-label",
+    selector: "LabeledStatement",
     message:
       "Labels are a form of GOTO; using them makes code confusing and hard both to maintain and understand.",
-    selector: "LabeledStatement",
   },
   {
     name: "no-for-in-loops",
+    selector: "ForInStatement",
     message:
       "`for`-`in` loops skip array holes, return array indices as strings, and may visit the prototype chain or other enumerable properties, which is virtually never what you want. Use a more robust iteration method such as `for`-`of`; if not iterating over an array, convert to an array first, perhaps using Object.{keys,values,entries}.",
-    selector: "ForInStatement",
   },
   {
     name: "no-reflect",
+    selector: "Identifier[name='Reflect']",
     message:
       "Avoid the Reflect API. It is a very low level feature that is rarely needed, except in complex libraries. There is no need to use this feature in normal code.",
-    selector: "Identifier[name='Reflect']",
   },
   {
     name: "no-proxy",
+    selector: "Identifier[name='Proxy']",
     message:
       "Avoid the Proxy API. It is a low level feature that is rarely needed. In the niche cases when this is needed, feel free to disable this rule with the inline eslint-disable syntax.",
-    selector: "Identifier[name='Proxy']",
   },
   {
     name: "no-in-operator",
+    selector: "BinaryExpression[operator='in']",
     message:
       "Avoid the 'in' operator. In most cases, basic property access is all you need. For other cases, use Object.hasOwn(). In niche cases where you need to check for the existence of a property both on an object itself AND its prototype chain, feel free to disable this rule with the inline eslint-disable syntax.",
-    selector: "BinaryExpression[operator='in']",
   },
   {
     name: "no-access-modifiers",
-    message:
-      "Avoid access modifiers; prefer #private members or omitting the modifier.",
     selector: [
       ":matches(PropertyDefinition, MethodDefinition, TSParameterProperty)[accessibility]",
     ],
+    message:
+      "Avoid access modifiers; prefer #private members or omitting the modifier.",
   },
   {
     name: "no-delete-operator",
-    message: "Avoid `delete`; prefer destructuring.",
     selector: "UnaryExpression[operator='delete']",
+    message: "Avoid `delete`; prefer destructuring.",
   },
   {
-    name: "no-enums",
     // TODO: When microsoft/TypeScript#61414 lands, prefer enum annotations.
-    message: "Avoid `enum`; prefer using objects instead.",
+    name: "no-enums",
     selector: "TSEnumDeclaration",
+    message: "Avoid `enum`; prefer using objects instead.",
   },
 ];
 
